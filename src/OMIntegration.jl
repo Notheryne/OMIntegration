ee58a43212b4a92082261a3157c7c3e06581a102
Notheryne <rydzinski.bartosz.1998@gmail.com>
2020-05-07 14:54:30 +0200

    Fix tests

--- src/OMIntegration.jl
+++ src/OMIntegration.jl
@@ -3,7 +3,7 @@ module OMIntegration
 integration_func(x) = x^3 + x
 integration_func2(x) = x^2
 
-export integration_func(x)
-export integrationc_func2(x)
+export integration_func
+export integrationc_func2
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using OMIntegration
 using Test
 
 @testset "OMIntegration.jl" begin
-	@integration_func(2) = 10
-	@integration_func(1) = 2
-	@integration_func(-1) = -2
+	@test integration_func(2) == 10
+	@test integration_func(1) == 2
+	@test integration_func(-1) == -2
 end
